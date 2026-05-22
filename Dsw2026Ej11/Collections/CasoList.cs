cf0bf6c910fe6d55324a2d169a4b4becd6952ccf
Milena Coronel <coronelanamilena@gmail.com>
2026-05-22 12:15:13 -0300

    Caso List resuelto con ejemplos

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,8 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+using System.Diagnostics;
+
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +12,36 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> alumnos = new List<Alumno>();
+  
+   
+
+public void AgregarAlumno (Alumno nuevoAlumno)
+    {
+        alumnos.Add(nuevoAlumno);
+    }
+
+public List<Alumno> RetornaLista()
+    {
+        return alumnos;
+    }
+
+    public Alumno BuscarAlumno(string nombre) => alumnos.Find(alumno => alumno.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+
+
+    public bool EliminarAlumno (Alumno alumnoEliminar)
+    {
+        return alumnos.Remove(alumnoEliminar);
+    }
+
+public void EliminarAlumnoPosicion(int posicion)
+    {
+        if (posicion >= 0 && posicion < alumnos.Count)
+        {
+            alumnos.RemoveAt(posicion);
+        }
+        
+    }
+
+
 }
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,16 +1,71 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
 {
-    //Agregar 3 alumnos a la lista
-    //Listar por consola los alumnos
-    //Buscar por nombre un alumno que exista y mostrar por consola
-    //Buscar por nombre un alumno que no exista y mostrar por consola el texto "No existe"
-    //Eliminar un alumno y listar por consola los alumnos
-    //Eliminar el primer elemento de la lista y listar por consola los alumnos
+    
     public static void EjemploList()
     {
+        CasoList gestor = new CasoList();
+
+        //Agregar 3 alumnos a la lista
+        Alumno a1 = new Alumno(1, "Morena Garcia", 9.0);
+        Alumno a2 = new Alumno(2, "Pablo Perez", 6.7);
+        Alumno a3 = new Alumno(3, "Anastacia Lopez", 8.3);
+        gestor.AgregarAlumno(a1);
+        gestor.AgregarAlumno(a2);
+        gestor.AgregarAlumno(a3);
+
+        //Listar por consola los alumnos
+        Console.WriteLine("Lista de Alumnos");
+        List<Alumno> listaActual = gestor.RetornaLista();
+        foreach (Alumno alumno in listaActual)
+        {
+            Console.WriteLine(alumno);
+        }
+        Console.WriteLine();
+
+        //Buscar por nombre un alumno que exista y mostrar por consola
+        Console.WriteLine("Buscando al alumno Anastacia Lopez");
+        Alumno alumnoEncontrado = gestor.BuscarAlumno("anastacia lopez");
+
+        if (alumnoEncontrado != null)
+            Console.WriteLine($"Encontrado: {alumnoEncontrado}");
+        else 
+            Console.WriteLine("No existe en el listado");
+            Console.WriteLine();
+
+        //Buscar por nombre un alumno que no exista y mostrar por consola el texto "No existe"
+        Console.WriteLine("Buscando al alumno Maria Marta Ruiz");
+        Alumno alumnoInexistente = gestor.BuscarAlumno("maria marta ruiz");
+
+        if (alumnoInexistente != null)
+        {
+            Console.WriteLine($"Encontrado: {alumnoInexistente}");
+        }
+        else
+            Console.WriteLine("No existe"); 
+            Console.WriteLine();
+
+        //Eliminar un alumno y listar por consola los alumnos
+        Console.WriteLine("Eliminando al alumno Pablo Perez de la lista");
+        gestor.EliminarAlumno(a2);
+
+        foreach (Alumno alumno in gestor.RetornaLista())
+        {
+            Console.WriteLine(alumno);
+        }
+        Console.WriteLine();
+
+        //Eliminar el primer elemento de la lista y listar por consola los alumnos
+        Console.WriteLine("Eliminando al primer alumno del listado");
+        gestor.EliminarAlumnoPosicion(0); 
+
+        foreach (Alumno alumno in gestor.RetornaLista())
+        { Console.WriteLine(alumno); }
 
     }
 
